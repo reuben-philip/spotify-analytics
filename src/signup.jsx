7692df19c94ec071ee7ffd99f0251571cd797090
reuben-philip <reubenaphilip@gmail.com>
2025-04-22 19:00:50 -0700

    feat: updated login and signup pages

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,22 +1,72 @@
-import React from 'react';
-import './signup.css'; 
+import React, { useState } from 'react';
+import './signup.css';
 
-const Signup = () => {
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Login attempt:', formData);
+  };
+
   return (
-    <div className="login-wrapper">
-      <form className="form">
-        <span className="input-span">
-          <label htmlFor="email" className="label">Email</label>
-          <input type="email" name="email" id="email" />
-        </span>
-        <span className="input-span">
-          <label htmlFor="password" className="label">Password</label>
-          <input type="password" name="password" id="password" />
-        </span>
-        <span className="span"><a href="#">Forgot password?</a></span>
-        <input className="submit" type="submit" value="Log in" />
-        <span className="span">Don't have an account? <a href="signup.jsx">Sign up</a></span>
-      </form>
+    <div className="login-page">
+      <div className="bubbles-container">
+        {/* Generate multiple bubble elements */}
+        {[...Array(15)].map((_, index) => (
+          <div key={index} className="bubble"></div>
+        ))}
+      </div>
+      
+      <div className="login-box">
+        <h1>Sign Up!</h1>
+        
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="Enter your username"
+              required
+            />
+          </div>
+          
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          
+          <div className="options-row">
+            <a href="#forgot" className="forgot-link">Forgot Password?</a>
+          </div>
+          
+          <button type="submit" className="login-button">Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 };
